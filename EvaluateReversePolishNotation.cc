dd0294620b1aee66500cf2703cf2aba79ee7d9d0
Da-Cheng <inoutmoto@gmail.com>
2013-12-27 14:59:07 -0500

    modified:   EvaluateReversePolishNotation.cc

--- EvaluateReversePolishNotation.cc
+++ EvaluateReversePolishNotation.cc
@@ -15,7 +15,7 @@ using namespace std;
 class Solution
 {
     public:
-        // Using a stack. Consider only valid expression due to prob. description. 
+        // Using a stack. Consider only valid expression. 
         // (1) Operand input: push_back 
         // (2) Operator input: pop_back two operands, evaluate, and push the result back 
         // Time: O(n)
