95b19d1fe87bc2d1a5df013090292e4c19e23321
Da-Cheng <inoutmoto@gmail.com>
2013-12-27 14:54:21 -0500

    add EvaluateReversePolishNotation.cc

---
+++ EvaluateReversePolishNotation.cc
@@ -0,0 +1,66 @@
+// Problem description:
+// http://oj.leetcode.com/problems/evaluate-reverse-polish-notation/
+// Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+// Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+// Some examples:
+// ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+// ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+    public:
+        // Using a stack. 
+        // (1) Operand input: push_back 
+        // (2) Operator input: pop_back two operands, evaluate, and push the result back 
+        // Time: O(n)
+        // Space: O(n)
+        int evalRPN(vector<string> &tokens) 
+        {
+            vector<int> stack;
+            for(int i = 0; i < tokens.size(); i++)
+            {
+                try
+                {
+                    int op = stoi(tokens[i], NULL, 10); //convert a string token into number
+                    stack.push_back(op);
+                }
+                catch(const std::invalid_argument& ia) // exception: operators (+-*/) found
+                {
+                    //cerr << "Operators found due to " << ia.what() << "\n";
+                    int op2 = stack.back();
+                    stack.pop_back();
+                    int op1 = stack.back();
+                    stack.pop_back();
+                    if(tokens[i] == "+")
+                        stack.push_back(op1+op2);
+                    else if(tokens[i] == "-")
+                        stack.push_back(op1-op2);
+                    else if(tokens[i] == "*")
+                        stack.push_back(op1*op2);
+                    else
+                        stack.push_back(op1/op2); // Consider only int operation due to prob. description 
+                }
+            }
+            return stack.back();
+        }
+};
+
+int main(int argc, char* argv[])
+{
+    Solution sol;
+    // test benchmark
+    vector<string> tokens;
+    tokens.push_back("4");
+    tokens.push_back("13");
+    tokens.push_back("5");
+    tokens.push_back("/");
+    tokens.push_back("+");
+    cout << sol.evalRPN(tokens) << "\n";
+    return 1;
+}
--- LRUCache.cc
+++ LRUCache.cc
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <unordered_map>
-#include <climits>
-
-using namespace std;
-
 // Problem description: 
 // http://oj.leetcode.com/problems/lru-cache/
 // Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
 // get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
 // set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+#include <iostream>
+#include <unordered_map>
+#include <climits>
+
+using namespace std;
 
 class LRUCache
 {
